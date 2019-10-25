dc796c4855e1d6b0ce6ed65469dd4e099acedee1
kejiefu <kejiefu@126.com>
2019-10-25 16:58:16 +0800

    添加了代理模式

---
+++ src/main/java/com/design/pattern/adapter/AdapterPatternDemo.java
@@ -0,0 +1,100 @@
+package com.design.pattern.adapter;
+
+
+/**
+ * 菜鸟教程
+ * 这种模式涉及到一个单一的类，该类负责加入独立的或不兼容的接口功能。举个真实的例子，读卡器是作为内存卡和笔记本之间的适配器。您将内存卡插入读卡器，再将读卡器插入笔记本，这样就可以通过笔记本来读取内存卡。
+ * 我们有一个 MediaPlayer 接口和一个实现了 MediaPlayer 接口的实体类 AudioPlayer。默认情况下，AudioPlayer 可以播放 mp3 格式的音频文件。
+ * 我们还有另一个接口 AdvancedMediaPlayer 和实现了 AdvancedMediaPlayer 接口的实体类。该类可以播放 vlc 和 mp4 格式的文件。
+ * 我们想要让 AudioPlayer 播放其他格式的音频文件。为了实现这个功能，我们需要创建一个实现了 MediaPlayer 接口的适配器类 MediaAdapter，并使用 AdvancedMediaPlayer 对象来播放所需的格式。
+ * AudioPlayer 使用适配器类 MediaAdapter 传递所需的音频类型，不需要知道能播放所需格式音频的实际类。AdapterPatternDemo，我们的演示类使用 AudioPlayer 类来播放各种格式。
+ */
+public class AdapterPatternDemo {
+    public static void main(String[] args) {
+        AudioPlayer audioPlayer = new AudioPlayer();
+
+        audioPlayer.play("mp3", "beyond the horizon.mp3");
+        audioPlayer.play("mp4", "alone.mp4");
+        audioPlayer.play("vlc", "far far away.vlc");
+        audioPlayer.play("avi", "mind me.avi");
+    }
+}
+
+interface MediaPlayer {
+     void play(String audioType, String fileName);
+}
+
+interface AdvancedMediaPlayer {
+     void playVlc(String fileName);
+
+     void playMp4(String fileName);
+}
+
+class VlcPlayer implements AdvancedMediaPlayer {
+    @Override
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: " + fileName);
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        //什么也不做
+    }
+}
+
+class Mp4Player implements AdvancedMediaPlayer {
+
+    @Override
+    public void playVlc(String fileName) {
+        //什么也不做
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        System.out.println("Playing mp4 file. Name: " + fileName);
+    }
+}
+
+class MediaAdapter implements MediaPlayer {
+
+    AdvancedMediaPlayer advancedMusicPlayer;
+
+    public MediaAdapter(String audioType) {
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMusicPlayer = new VlcPlayer();
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMusicPlayer = new Mp4Player();
+        }
+    }
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMusicPlayer.playVlc(fileName);
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMusicPlayer.playMp4(fileName);
+        }
+    }
+}
+
+class AudioPlayer implements MediaPlayer {
+    MediaAdapter mediaAdapter;
+
+    @Override
+    public void play(String audioType, String fileName) {
+
+        //播放 mp3 音乐文件的内置支持
+        if (audioType.equalsIgnoreCase("mp3")) {
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        }
+        //mediaAdapter 提供了播放其他文件格式的支持
+        else if (audioType.equalsIgnoreCase("vlc")
+                || audioType.equalsIgnoreCase("mp4")) {
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        } else {
+            System.out.println("Invalid media. " +
+                    audioType + " format not supported");
+        }
+    }
+}
---
+++ src/main/java/com/design/pattern/proxy/ProxyTest.java
@@ -0,0 +1,45 @@
+package com.design.pattern.proxy;
+
+/**
+ * 代理模式的定义：由于某些原因需要给某对象提供一个代理以控制对该对象的访问。这时，访问对象不适合或者不能直接引用目标对象，代理对象作为访问对象和目标对象之间的中介。
+ */
+public class ProxyTest {
+    public static void main(String[] args) {
+        Proxy proxy = new Proxy();
+        proxy.Request();
+    }
+}
+
+//抽象主题
+interface Subject {
+    void Request();
+}
+
+//真实主题
+class RealSubject implements Subject {
+    public void Request() {
+        System.out.println("访问真实主题方法...");
+    }
+}
+
+//代理
+class Proxy implements Subject {
+    private RealSubject realSubject;
+
+    public void Request() {
+        if (realSubject == null) {
+            realSubject = new RealSubject();
+        }
+        preRequest();
+        realSubject.Request();
+        postRequest();
+    }
+
+    public void preRequest() {
+        System.out.println("访问真实主题之前的预处理。");
+    }
+
+    public void postRequest() {
+        System.out.println("访问真实主题之后的后续处理。");
+    }
+}
