1baf2817ccd6332ee72db0efd071b9ec10bb19e3
kejiefu <kejiefu@126.com>
2020-01-09 14:25:55 +0800

    补充完整依赖倒置原则

--- src/main/java/com/principle/DependenceInversionPrinciple.java
+++ src/main/java/com/principle/DependenceInversionPrinciple.java
@@ -1,6 +1,20 @@
 package com.principle;
 
 /**
+ * 依赖倒置原则
+ * 依赖倒置原则的主要作用如下。
+ * 依赖倒置原则可以降低类间的耦合性。
+ * 依赖倒置原则可以提高系统的稳定性。
+ * 依赖倒置原则可以减少并行开发引起的风险。
+ * 依赖倒置原则可以提高代码的可读性和可维护性。
+ * <p>
+ * 依赖倒置原则的实现方法
+ * 依赖倒置原则的目的是通过要面向接口的编程来降低类间的耦合性，所以我们在实际编程中只要遵循以下4点，就能在项目中满足这个规则。
+ * 每个类尽量提供接口或抽象类，或者两者都具备。
+ * 变量的声明类型尽量是接口或者是抽象类。
+ * 任何类都不应该从具体类派生。
+ * 使用继承时尽量遵循里氏替换原则。
+ *
  * @Auther kejiefu
  * @Date 2019/10/12 0012
  */
@@ -16,9 +30,10 @@ public class DependenceInversionPrinciple {
 
 //商店
 interface Shop {
-     String sell(); //卖
+    String sell(); //卖
 }
 
+//任何类都不应该从具体类派生。
 //韶关网店
 class ShaoguanShop implements Shop {
     public String sell() {
@@ -35,6 +50,8 @@ class WuyuanShop implements Shop {
 
 //顾客
 class Customer {
+
+    //变量的声明类型尽量是接口或者是抽象类。
     public void shopping(Shop shop) {
         //购物
         System.out.println(shop.sell());
